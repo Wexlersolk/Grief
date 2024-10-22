8fe972a9813f2300dac81569cadd34a82f8999c5
Wexler763 <danylkarp@gmail.com>
2024-10-22 08:57:54 +0300

    feat: add rate limiter impl using fixed window algorithm

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,51 @@
 package main
 
+import (
+	"time"
+
+	"github.com/Wexlersolk/GriefBlades/internal/env"
+	"github.com/nxadm/tail/ratelimiter"
+)
+
 const version = "1.1.0"
 
-func main() { 
-	cfg:= config (
-		addr: env.GetString("ADDR", "8080"),
-	)
-}
+func main() {
+	cfg := config{
+		addr:        env.GetString("ADDR", "8080"),
+		apiURL:      env.GetString("EXTERNAL_URL", "localhost:8080"),
+		frontendURL: env.GetString("FRONTEND_URL", "http://localhost:8181"),
+		db: dbConfig{
+			addr:         env.GetString("DBADDR", "you can enter your db adress here"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "5m"),
+		},
+		redisCfg: redisConfig{
+			addr:    env.GetString("REDDIS_ADDR", "localhost:5173"),
+			pw:      env.GetString("REDDIS_PW", ""),
+			db:      env.GetInt("REDDIS_DB", 0),
+			enabled: env.GetBool("REDDIS_ENABLED", false),
+		},
 
+		env: env.GetString("ENV", "development"),
+		mail: mailConfig{
+			exp:       time.Hour * 24,
+			fromEmail: env.GetString("FROM_EMAIL", ""),
+			sendGrid: sendGridConfig{
+				apiKey: env.GetString("SEND_API_KEY", ""),
+			},
+		},
+		auth: authConfig{
+			basic: basicConfig{
+				user: env.GetString("AUTH_BASIC_USER", "admin"),
+				pass: env.GetString("AUTH_BASIC_PASS", "admin"),
+			},
+			token: tokenConfig{
+				secret: env.GetString("AUTH_TOKEN_SECRET", "secret"),
+				exp:    time.Hour * 24,
+				iss:    "griefblades",
+			},
+		},
+		rateLimiter: ratelimiter.Config,
+	}
+}
--- go.mod
+++ go.mod
@@ -3,25 +3,8 @@ module github.com/Wexlersolk/GriefBlades
 go 1.23.1
 
 require (
-	github.com/go-chi/chi v1.5.5
-	github.com/go-chi/cors v1.2.1
-	github.com/swaggo/http-swagger v1.3.4
-	github.com/swaggo/swag v1.16.4
+	github.com/nxadm/tail v1.4.11
 	go.uber.org/zap v1.27.0
 )
 
-require (
-	github.com/KyleBanks/depth v1.2.1 // indirect
-	github.com/go-openapi/jsonpointer v0.19.5 // indirect
-	github.com/go-openapi/jsonreference v0.20.0 // indirect
-	github.com/go-openapi/spec v0.20.6 // indirect
-	github.com/go-openapi/swag v0.19.15 // indirect
-	github.com/josharian/intern v1.0.0 // indirect
-	github.com/mailru/easyjson v0.7.6 // indirect
-	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
-	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/tools v0.7.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-)
+require go.uber.org/multierr v1.10.0 // indirect
