3e668e4d93ad4aff792e74e87d5fc65a55200e26
Wexler763 <danylkarp@gmail.com>
2024-10-24 23:46:47 +0300

    feat: mailer send func

--- internal/mailer/sendgrid.go
+++ internal/mailer/sendgrid.go
@@ -1,6 +1,14 @@
 package mailer
 
-import "github.com/sendgrid/sendgrid-go"
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"time"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
 
 type SendGridMailer struct {
 	fromEmail string
@@ -16,4 +24,48 @@ func NewSendgrid(apiKey, fromEmail string) *SendGridMailer {
 		apiKey:    apiKey,
 		client:    client,
 	}
+}
+
+func (m *SendGridMailer) Send(templateFile, username, email string, data any, isSandbox bool) (int, error) {
+	from := mail.NewEmail(FromName, m.fromEmail)
+	to := mail.NewEmail(username, email)
+
+	tmpl, err := template.ParseFS(FS, "templates/"+templateFile)
+	if err != nil {
+		return -1, err
+	}
+
+	subject := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(subject, "subject", data)
+	if err != nil {
+		return -1, err
+	}
+
+	body := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(subject, "body", data)
+	if err != nil {
+		return -1, err
+	}
+
+	message := mail.NewSingleEmail(from, subject.String(), to, "", body.String())
+
+	message.SetMailSettings(&mail.MailSettings{
+		SandboxMode: &mail.Setting{
+			Enable: &isSandbox,
+		},
+	})
+
+	var retryErr error
+	for i := 0; i < maxRetries; i++ {
+		responce, retryErr := m.client.Send(message)
+		if retryErr != nil {
+			time.Sleep(time.Second * time.Duration(i+1))
+			continue
+		}
+
+		return responce.StatusCode, nil
+	}
+
+	return -1, fmt.Errorf("failed to send email after %d attempt, error: %v", maxRetries, retryErr)
+
 }
