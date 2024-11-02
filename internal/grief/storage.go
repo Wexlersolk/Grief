dd4a8eb192eae8416ebf3adc34921f4ba9c4a8f3
Wexler763 <danylkarp@gmail.com>
2024-11-02 07:30:05 +0200

    feat: setup initial interfaces

--- internal/grief/storage.go
+++ internal/grief/storage.go
@@ -2,6 +2,7 @@ package grief
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"time"
 )
@@ -19,5 +20,24 @@ type Storage struct {
 		Delete(context.Context, int64) error
 		Update(context.Context, *Post) error
 		GetUserFeed(context.Context, int64, PaginatedFeedQuary) ([]PostWithMetadata, error)
+	}
+	Users interface {
+		GetByID(context.Context, int64) (*User, error)
+		GetByEmail(context.Context, string) (*User, error)
+		Create(context.Context, *sql.Tx, *User) error
+		CreateAndInvite(ctx context.Context, user *User, token string, exp time.Duration) error
+		Activate(context.Context, string) error
+		Delete(context.Context, int64) error
+	}
+	Comments interface {
+		Create(context.Context, *Comment) error
+		GetByPostID(context.Context, int64) ([]Comment, error)
+	}
+	Followers interface {
+		Follow(ctx context.Context, followerID, userID int64) error
+		Unfollow(ctx context.Context, followerID, userID int64) error
+	}
+	Roles interface {
+		GetByName(context.Context, string) (*Role, error)
 	}
 }
