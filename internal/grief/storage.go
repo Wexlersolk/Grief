f23867f5a3d0be724deaa8bd94449ffddebc2109
Wexler763 <danylkarp@gmail.com>
2024-11-02 07:38:25 +0200

    feat: add a helper func for managing transactions

--- internal/grief/storage.go
+++ internal/grief/storage.go
@@ -41,3 +41,27 @@ type Storage struct {
 		GetByName(context.Context, string) (*Role, error)
 	}
 }
+
+func NewStorage(db *sql.DB) Storage {
+	return Storage{
+		Posts:     &PostStore{db},
+		Users:     &UserStore{db},
+		Comments:  &CommenStore{db},
+		Followers: &FollowerStore{db},
+		Roles:     &RoleStore{db},
+	}
+}
+
+func withTx(db *sql.DB, ctx context.Context, fn func(*sql.Tx) error) error {
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	if err := fn(tx); err != nil {
+		_ = tx.Rollback()
+		return err
+	}
+
+	return tx.Commit()
+}
